bfd29ce34449a47ab864ea57630d029371a584c8
Bigo <bigo@crisidev.org>
2018-02-04 14:43:49 +0100

    Bump version to v0.3.3

--- chunkwm.rb
+++ chunkwm.rb
@@ -1,16 +1,16 @@
 class Chunkwm < Formula
   desc "Tiling window manager for macOS based on plugin architecture"
   homepage "https://github.com/koekeishiya/chunkwm"
-  url "https://github.com/koekeishiya/chunkwm/archive/v0.3.2.tar.gz"
-  sha256 "6c56e6c8cf29113b517c9601bd0328f9d65dd68b9d313ce94dfba1ac2c24f581"
+  url "https://github.com/koekeishiya/chunkwm/archive/v0.3.3.tar.gz"
+  sha256 "f77711d4b4d6a49927b125f077334efb4e4633e33bf23689d3874c0c40f07e66"
   head "https://github.com/koekeishiya/chunkwm.git"
 
   option "without-tiling", "Do not build tiling plugin."
   option "without-ffm", "Do not build focus-follow-mouse plugin."
   option "without-border", "Do not build border plugin."
   option "with-purify", "Build purify plugin."
-  option "with-logging", "Redirect stdout and stderr to log files to standard brew path"
-  option "with-tmp-logging", "Redirect stdout and stderr to /tmp"
+  option "with-logging", "Deprecated, here for backward compatibility, does not have effect."
+  option "with-tmp-logging", "Deprecated, here for backward compatibility, does not have effect."
 
   depends_on :macos => :el_capitan
 
@@ -56,14 +56,12 @@ class Chunkwm < Formula
 
     This has to be done after every update to chunkwm, unless you codesign the
     binary with self-signed certificate before restarting
-      Create code signing certificate named "chunkwm-cert" using Keychain Access.app
+    Create code signing certificate named "chunkwm-cert" using Keychain Access.app
       codesign -fs "chunkwm-cert" #{opt_bin}/chunkwm
 
-    If the formula has been built with --with-logging, logs will be found in
-      #{var}/log/chunkwm/chunkwm.[out|err].log
-  
-    If the formula has been built with --with-tmp-logging, logs will be found in
-      /tmp/chunkwm.[out|err].log
+    NOTE: options "--with-logging" and "--with-tmp-logging" are deprecated since now
+    chunkwm supports logging through configuration:
+      chunkc core::log_file <stdout | stderr | /path/to/file>
 
     NOTE: plugins folder has been moved to #{opt_pkgshare}/plugins
     EOS
@@ -71,89 +69,29 @@ class Chunkwm < Formula
 
   plist_options :manual => "chunkwm"
 
-  if build.with? "logging"
-    def plist; <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/chunkwm</string>
+      </array>
+      <key>EnvironmentVariables</key>
       <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/chunkwm</string>
-        </array>
-        <key>EnvironmentVariables</key>
-        <dict>
-          <key>PATH</key>
-          <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-        </dict>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/chunkwm/chunkwm.out.log</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/chunkwm/chunkwm.err.log</string>
+        <key>PATH</key>
+        <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
       </dict>
-      </plist>
-      EOS
-    end
-  elsif build.with? "tmp-logging"
-    def plist; <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/chunkwm</string>
-        </array>
-        <key>EnvironmentVariables</key>
-        <dict>
-          <key>PATH</key>
-          <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-        </dict>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardOutPath</key>
-        <string>/tmp/chunkwm.out.log</string>
-        <key>StandardErrorPath</key>
-        <string>/tmp/chunkwm.err.log</string>
-      </dict>
-      </plist>
-      EOS
-    end
-  else
-    def plist; <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/chunkwm</string>
-        </array>
-        <key>EnvironmentVariables</key>
-        <dict>
-          <key>PATH</key>
-          <string>#{HOMEBREW_PREFIX}/bin:/usr/bin:/bin:/usr/sbin:/sbin</string>
-        </dict>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-      </dict>
-      </plist>
-      EOS
-    end
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
   end
 
   test do
